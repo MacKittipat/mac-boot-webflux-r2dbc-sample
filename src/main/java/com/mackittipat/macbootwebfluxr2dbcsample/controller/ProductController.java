c9a2397b3d44e818165a9941e0fe645206c206b2
Mac <mac.kittipat@gmail.com>
2022-04-15 13:27:07 +0700

    Add sample endpoint for file upload

--- src/main/java/com/mackittipat/macbootwebfluxr2dbcsample/controller/ProductController.java
+++ src/main/java/com/mackittipat/macbootwebfluxr2dbcsample/controller/ProductController.java
@@ -4,17 +4,21 @@ import com.mackittipat.macbootwebfluxr2dbcsample.model.Product;
 import com.mackittipat.macbootwebfluxr2dbcsample.repository.ProductRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.buffer.DataBufferUtils;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.codec.multipart.FilePart;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.nio.charset.StandardCharsets;
 import java.time.Duration;
 import java.time.LocalTime;
 import java.util.UUID;
@@ -49,4 +53,14 @@ public class ProductController {
                 .map(sequence -> "Product - " + LocalTime.now().toString());
     }
 
+    @PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public Flux<String> upload(@RequestPart("files") Flux<FilePart> filePartFlux) {
+        return filePartFlux.flatMap(filePart ->
+                filePart.content().map(dataBuffer -> {
+                    byte[] bytes = new byte[dataBuffer.readableByteCount()];
+                    dataBuffer.read(bytes);
+                    DataBufferUtils.release(dataBuffer);
+                    return new String(bytes, StandardCharsets.UTF_8);
+                }));
+    }
 }
